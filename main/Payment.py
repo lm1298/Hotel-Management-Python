9ea20b6e0d6353e067fbb6d82ced7edcf96744d1
JohnGant19 <jbg1025@wildcats.unh.edu>
2023-04-19 08:36:55 -0400

    Update to Payment and Staff

--- main/Payment.py
+++ main/Payment.py
@@ -12,19 +12,17 @@ class Payment:
         self.is_paid = True
         print(f"{self.guest.name} has paid {self.amount}.")
 
-    def get_payment_details(self):
+    def get_payment_details(self): 
         if self.is_paid:
-            status = "Paid" 
+            status = "Paid"
         else: "Unpaid"
-
         return f"Guest: {self.guest.name}, Amount: {self.amount}, Status: {status}"
 
-guest = Guest("John Smith")
+def process_payment(guest_name, amount):
+    guest = Guest(guest_name)
+    payment = Payment(guest, amount)
+    payment.make_payment()
+    return payment.get_payment_details()
 
-payment = Payment(guest, 100)
-
-payment.make_payment()
-
-details = payment.get_payment_details()
-
+details = process_payment("John Smith", 100)
 print(details)
