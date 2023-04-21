9d56290f1a86e481d948a3eb5bec9065ca19f161
lm1298 <lm1298@usnh.edu>
2023-04-21 16:04:38 -0400

    design Proposal for hotel management project

---
+++ PROPOSAL.md
@@ -0,0 +1,39 @@
+## Final Project Proposal
+### Project Name: Hotel Management System
+
+### Team Members:
+Lakshmi Prasanna Malempati  
+Sujan Varma Guntimadugu  
+John Gant
+
+### Outline of Hotel Management System Project:
+#### Project Overview: 
+* The hotel management project aims to create a system that
+manages room bookings, check room availability , staff, payment, and guest information for a hotel.
+* The project includes six classes - Room, Guest, Staff, Payment, Booking  and Hotel.
+* The Room class contains attributes related to the room such as room_number, room_type, price, occupancy, availability status.
+* Guest class contains attributes related to the guest such as his name, email and phone number .
+* Hotel class contains hotel name, list of rooms and guests. 
+
+#### Project Complexity and Outcome:
+* The project is of intermediate complexity and aims to create a functional hotel
+management system.
+* The outcome will be a Python program that can manage room bookings, check-ins, checkouts, and guest information.
+
+* Major Features of the Project Code:
+  * Room booking: The program should allow the user to book a room by providing the room number.
+  * Room availability: The program should allow the user to check the availability of a specific room type. 
+  * Guest information: The program should allow the user to add guest
+  information and view their stays.
+  * Check-out: The program should allow the user to check out a guest by providing the room number.  
+
+### The project includes 6 classes :
+Room, Guest, Booking, Staff, Payment and Hotel.
+* The Room class represents a hotel room and has the following attributes:
+ 
+  * room_number: the room number
+  * room_type: the type of the room (single, double, etc.)
+  * occupancy: the number of people that can occupy the room
+  * price: the price per night
+  * is_available: a boolean flag indicating whether the room is currently available for booking.
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from datetime import date
-from main.room import Room
-from main.booking import Booking
+from datetime import datetime
+from src.room import Room
+from src.booking import Booking
 
 # Initialize a Room object
 room1 = Room(101, "Single", 1, 50)
-
+room2 = Room(102, 'single', 2, 100)
 # Initialize a Booking object
-booking1 = Booking("John Smith", room1, date(2023, 5, 1), date(2023, 5, 7))
+booking1 = Booking("prasanna", room1, "2023-09-08", "2023-09-11")
 
 
 # Define a function to display the menu
@@ -24,35 +24,66 @@ def display_menu():
 # Define a function to handle user input and execute commands
 def handle_input(choice):
     if choice == "1":
-        checkin_date = input("Check-in date (YYYY-MM-DD): ")
-        checkout_date = input("Check-out date (YYYY-MM-DD): ")
-        if room1.check_availability(checkin_date, checkout_date):
-            print("Room is available.")
-        else:
-            print("Room is not available.")
+        while True:
+            checkin_date = input("Check-in date (YYYY-MM-DD): ")
+            checkout_date = input("Check-out date (YYYY-MM-DD): ")
+            try:
+                datetime.strptime(checkin_date, '%Y-%m-%d')
+                datetime.strptime(checkout_date, '%Y-%m-%d')
+            except ValueError:
+                print("Invalid date format. Please try again.")
+                continue
+            if datetime.strptime(checkin_date, '%Y-%m-%d') >= datetime.strptime(checkout_date,
+                                                                                '%Y-%m-%d'):
+                print("Check-out date must be after check-in date. Please try again.")
+                continue
+            if room1.check_availability(checkin_date, checkout_date):
+                print("Room is available.")
+            else:
+                print("Room is not available.")
+            break
     elif choice == "2":
-        guest_name = input("Guest name: ")
-        checkin_date = input("Check-in date (YYYY-MM-DD): ")
-        checkout_date = input("Check-out date (YYYY-MM-DD): ")
-        booking = Booking(guest_name, room1, checkin_date, checkout_date)
-        if booking.make_booking(guest_name, room1, checkin_date, checkout_date):
-            print("Reservation made.")
-        else:
-            print("Room is not available.")
+        while True:
+            guest_name = input("Guest name: ")
+            room = input("enter room number:")
+            checkin_date = input("Check-in date (YYYY-MM-DD): ")
+            checkout_date = input("Check-out date (YYYY-MM-DD): ")
+            try:
+                datetime.strptime(checkin_date, '%Y-%m-%d')
+                datetime.strptime(checkout_date, '%Y-%m-%d')
+            except ValueError:
+                print("Invalid date format. Please try again.")
+                continue
+            if datetime.strptime(checkin_date, '%Y-%m-%d') >= datetime.strptime(checkout_date,
+                                                                                      '%Y-%m-%d'):
+                print("Check-out date must be after check-in date. Please try again.")
+                continue
+            #booking = Booking(guest_name, room, checkin_date, checkout_date)
+            if room1.book_room():
+                print("Reservation made.")
+            else:
+                print("Room is not available.")
+            break
     elif choice == "3":
         reservations = room1.get_reservations()
         if len(reservations) == 0:
             print("There are no reservations for this room.")
         else:
-            for i, reservation in enumerate(reservations):
-                print(f"{i + 1}. {reservation}")
-            choice = input("Enter the number of the reservation to cancel: ")
-            try:
-                choice = int(choice)
-                reservation = reservations[choice - 1]
-                room1.cancel_reservation(reservation)
-            except (ValueError, IndexError):
-                print("Invalid choice.")
+            while True:
+                for i, reservation in enumerate(reservations):
+                    print(f"{i + 1}. {reservation}")
+                choice = input("Enter the number of the reservation to cancel: ")
+                try:
+                    choice = int(choice)
+                    if choice < 1 or choice > len(reservations):
+                        raise ValueError
+                    reservation = reservations[choice - 1]
+                    room1.cancel_reservation(reservation)
+                    print("Reservation cancelled.")
+                    break
+                except (ValueError, IndexError):
+                    print("Invalid choice. Please try again.")
+                    continue
     elif choice == "4":
         reservations = room1.get_reservations()
         if len(reservations) == 0:
@@ -64,11 +95,23 @@ def handle_input(choice):
         revenue = room1.get_revenue()
         print(f"Total revenue for this room: ${revenue}")
     elif choice == "6":
-        new_price = input("New price: ")
-        room1.update_price(new_price)
+        while True:
+            new_price = input("New price: ")
+            try:
+                new_price = float(new_price)
+            except ValueError:
+                print("Invalid price format. Please try again.")
+                continue
+            if new_price <= 0:
+                print("Price must be greater than 0. Please try again.")
+                continue
+            room1.update_price(new_price)
+            print("Price updated.")
+            break
     elif choice == "7":
-        new_occupancy = input("New occupancy: ")
-        room1.update_occupancy(new_occupancy)
+        while True:
+            new_occupancy = input("New occupancy: ")
+            room1.update_occupancy(new_occupancy)
     elif choice == "8":
         exit()
     else:
