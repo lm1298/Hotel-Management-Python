2b80703ded656a344faca6302abfb4111e13e849
sg1501 <sujanvarma.guntimaduguvenkata@unh.edu>
2023-05-01 18:36:02 -0400

    Added few changes to Test cases

--- tests/test_guest.py
+++ tests/test_guest.py
@@ -1,32 +1,49 @@
+import os
 import unittest
 from src.guest import Guest
 from src.room import Room
 
-
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("John Doe", "johndoe@example.com", "123-456-7890")
+        self.guest = Guest("John Smith", "johnsmith@example.com", "555-1234")
+        self.room = Room(101, 2, 100.0, False)
 
     def test_add_stay(self):
-        """
-        Test that a stay is added to the guest's record correctly.
-        """
-        self.guest.add_stay(self.room)
+        self.guest.add_stay(self.room, "2023-05-01", "2023-05-05")
+        self.assertEqual(len(self.guest.stays), 1)
+        self.assertEqual(self.guest.stays[0][0], self.room)
+        self.assertEqual(self.guest.stays[0][1], "2023-05-01")
+        self.assertEqual(self.guest.stays[0][2], "2023-05-05")
+
+    def test_get_total_stays(self):
+        self.assertEqual(self.guest.get_total_stays(), 0)
+        self.guest.add_stay(self.room, "2023-05-01", "2023-05-05")
         self.assertEqual(self.guest.get_total_stays(), 1)
 
     def test_get_last_stay(self):
-        """
-        Test that the last stay of the guest is retrieved correctly.
-        """
-        self.guest.add_stay(self.room)
-        self.assertEqual(self.guest.get_last_stay(), self.room)
+        self.assertIsNone(self.guest.get_last_stay())
+        self.guest.add_stay(Room(102, 2, 120.0, False), "2023-04-20", "2023-04-25")
+        self.guest.add_stay(Room(103, 3, 150.0, False), "2023-05-10", "2023-05-12")
+        last_stay = self.guest.get_last_stay()
+        self.assertIsNotNone(last_stay)
+        self.assertIn("Room 103", last_stay)
 
     def test_to_csv(self):
-        """
-        Test that the guest's information is saved to a CSV file correctly.
-        """
-        self.guest.to_csv("test_guest.csv")
-        # Assert that the file was created
-        with open("test_guest.csv") as f:
-            self.assertIsNotNone(f.read())
+        # Clear previous content in file
+        filename = "test_guest.csv"
+        with open(filename, mode="w", newline="") as file:
+            file.write("")
+        self.guest.to_csv(filename)
+
+        # Check if file exists and has correct content
+        file_exists = os.path.isfile(filename)
+        with open(filename) as file:
+            lines = file.readlines()
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(lines[0].strip(), "John Smith,johnsmith@example.com,555-1234")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
